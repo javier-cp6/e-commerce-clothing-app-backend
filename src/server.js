625831831719c8b95bbac6dbab672823f04b07d8
Andrew Medina Condo <henry.medina@tecsup.edu.pe>
2022-09-02 18:53:56 -0500

    first commit adding swagger

--- src/server.js
+++ src/server.js
@@ -7,6 +7,10 @@ import { usersRouter } from './routes/users.routes.js';
 import { cartsRouter } from './routes/carts.routes.js';
 import { ordersRouter } from './routes/orders.routes.js';
 
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './swagger.json' assert {type: 'json'};
+
+
 mercadopago.configure({
   access_token: process.env.MP_ACCESS_TOKEN,
   integrator_id: process.env.MP_INTEGRATOR_ID
@@ -17,7 +21,7 @@ const PORT = process.env.PORT;
 
 app.use(
   cors({
-    origin: [process.env.APP_URL, process.env.ADMIN_APP_URL],
+    origin: [process.env.APP_URL, process.env.ADMIN_APP_URL,,"https://editor.swagger.io"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["accept", "authorization", "content-type"]
   })
@@ -30,6 +34,7 @@ app.use(productsRouter)
 app.use(usersRouter)
 app.use(cartsRouter)
 app.use(ordersRouter)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(PORT, ()=> {
   console.log(`Server running on port ${PORT}`);
